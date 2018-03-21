182902e033609dfdc3ee6f28800a883f15c9f198
Jun <junfang219@gmail.com>
2018-03-20 22:58:39 -0400

    update all changes

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Memory Game Project
 
-## Table of Contents
+It is a classic memory game. To play the game, you need to click one card to flip it open. If two cards' signs are matched, they will stay opened. You win the game until you find all matching cards. Try to find all matches with minimum moves possible and your time will be recorded.
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
 
-## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+Click the link to play:
 
-To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
-## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+**External resources used:**
+
+[animate.css](https://daneden.github.io/animate.css/)
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
+var cards = document.querySelectorAll('.card');
+// it returns a nodeList
 
+// Convert Nodelist to an Array
+var cardsArray = Array.from(cards);
 
 /*
  * Display the cards on the page
@@ -25,6 +29,22 @@ function shuffle(array) {
     return array;
 }
 
+var shuffledCards = shuffle(cardsArray);
+
+// Remove previous cards and add new shuffed cards
+var deck = document.querySelector('.deck');
+
+for (let i = 0; i < cardsArray.length; i++) {
+	cardsArray[i].remove();
+}	
+
+for (let i = 0; i < shuffledCards.length; i++) {
+	deck.appendChild(shuffledCards[i]);
+}
+
+
+// Add event listener to the restart button
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +56,156 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+//Create array to record opened cards
+let openedCards = [];
+
+let moves = document.querySelector('.moves');
+
+let counter = 0;
+
+let lockDown = false;
+
+let flipOpen = ['open', 'show', 'animated', 'flipInY'];
+
+let stars = document.querySelector('.stars');
+
+let timer = document.querySelector('.time');
+
+let startTime;
+
+let gameStarted = false;
+
+let reportEverySec;
+
+let restart= document.querySelector('.restart');
+
+function FlipCard(elem) {
+	// Add class and then remove the class for the purpose of animation
+	elem.classList.add(...flipOpen);
+	setTimeout(function() {
+	 elem.classList.remove('ainmated', 'flipInY');
+	}, 200);
+}
+
+function ChangeBack(elem) {
+	elem.className = 'card';
+}
+
+function gameOver() {
+	return openedCards.length == 16;
+}
+
+function restartGame() {
+	window.location.reload();
+}
+
+restart.addEventListener('click',restartGame);
+
+// Track time
+function reportTime() {
+	let currTime = new Date().getTime() / 1000;
+	let totalTime = Math.floor(currTime - startTime);
+	timer.textContent = totalTime;
+	// modal report
+	document.getElementById('finaltime').textContent = totalTime;
+}
+
+
+function clickOpen(elem) {
+	for (let i = 0; i < elem.length; i++) {	
+		elem[i].addEventListener('click', async function(){
+			//if the card is already opened, exit the function
+			if (lockDown || elem[i].classList.contains('open')) {
+				return;
+			}
+
+			FlipCard(elem[i]);
+
+			openedCards.push(elem[i]);
+
+			// first time click a card, set gameStarted from F to T. Record the game start time
+			//and report ongoing time every 1 second.
+			if (!gameStarted) {
+				gameStarted = true;
+				startTime = new Date().getTime() / 1000;
+				reportEverySec = setInterval(reportTime, 1000);
+			}
+
+			//Compare two opened cards
+			let currTotal = openedCards.length;
+			if (currTotal % 2 == 0) {
+				//if openedCards array hold 2 cards then increment count and compare
+				counter += 1;
+
+				let last = openedCards[currTotal - 1];
+				let secondToLast = openedCards[currTotal - 2];
+				if (last.firstElementChild.className != secondToLast.firstElementChild.className) {
+					// if not match, shake and change the color of cards
+					last.classList.add('animated', 'shake', 'unmatch');
+					secondToLast.classList.add('animated', 'shake','unmatch');
+
+					// Delay 0.8s, during the time no cards can open			
+					let delay = new Promise(resolve => setTimeout(resolve, 800));
+					lockDown = true;
+					await delay;
+					lockDown = false;
+					
+					// Remove all classes
+					ChangeBack(last);
+					ChangeBack(secondToLast);
+					// Remove opened cards since they aren't matched and closed
+					openedCards.pop();
+					openedCards.pop();
+				} else {
+					last.classList.add('animated', 'jello','match');
+					// To sync time with previous setTimeout
+					setTimeout(function(){
+						secondToLast.classList.add('animated', 'jello','match');
+					}, 200);
+				}
+			}
+			// update moves
+			moves.textContent = counter;
+			document.getElementById('finalmove').textContent = counter;
+
+			// update score panel
+			switch (counter) {
+				case 9:
+					stars.lastElementChild.lastElementChild.className = 'fa fa-star-o';
+					break;
+				case 16:
+					stars.lastElementChild.previousElementSibling.firstElementChild.className = 'fa fa-star-o';
+					break;
+				case 28:
+					stars.firstElementChild.firstElementChild.className = 'fa fa-star-o';
+			}
+
+
+			// Congrats modal
+			let modal = document.getElementById('myModal');
+			// Get close buttom
+			let span = document.getElementsByClassName('close')[0];
+			
+			if (gameOver()) {
+				gameStarted = false;
+				clearInterval(reportEverySec);
+				modal.style.display = 'block';
+				// get restart button and add event listener
+				let restart_2 = document.querySelector('.modalRestart');
+				restart_2.addEventListener('click', restartGame)
+			}
+
+			//When the user clicks on (x), close the modal
+			span.onclick = function() {
+				modal.style.display = 'none';
+			}
+
+		});
+	}
+}
+
+
+
+clickOpen(shuffledCards);
